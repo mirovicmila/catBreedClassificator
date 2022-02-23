72808655858ca2085ddd3f66d10e8ecea36a74f6
mirovicmila <63522670+mirovicmila@users.noreply.github.com>
2022-02-23 10:53:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('/content/cat-breeds-model-augment.hdf5')
+  model=tf.keras.models.load_model('cat-breeds-model-augment.hdf5')
   return model
 
 
